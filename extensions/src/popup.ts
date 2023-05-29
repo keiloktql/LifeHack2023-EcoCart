275f3e9250d2b5c3c737e59c5f1a07f8c698b496
keilokimnida <tkl48leon@gmail.com>
2023-05-30 01:34:43 +0800

    [feat] testing out supabase login codes

--- extensions/src/popup.ts
+++ extensions/src/popup.ts
@@ -1,5 +1,83 @@
 import '../styles/popup.scss';
+import { createClient, User } from '@supabase/supabase-js';
+
+getCurrentUser().then((resp) => {
+  if (resp) {
+    console.log('user id:', resp.user.id);
+  } else {
+    console.log('user is not found');
+  }
+});
 
 document.getElementById('go-to-options').addEventListener('click', () => {
   chrome.runtime.openOptionsPage();
 });
+document
+  .querySelector('.login-btn-auth')
+  .addEventListener('click', async function () {
+    console.log('ran here 2');
+    await signInWithGoogle();
+  });
+
+const SUPABASE_PUBLIC_URL = 'https://raxkpyazhqcszrhtppyi.supabase.co';
+const SUPABASE_PUBLIC_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJheGtweWF6aHFjc3pyaHRwcHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUyNTY3OTksImV4cCI6MjAwMDgzMjc5OX0.RDomwou8YUMUufWN3CwOK0Rz_eHfQu5Tbvf-FF9ZiI4';
+export const supabase = createClient(
+  SUPABASE_PUBLIC_URL,
+  SUPABASE_PUBLIC_ANON_KEY,
+);
+
+// get auth url
+export async function signInWithGoogle() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+  });
+  console.log(data);
+
+  // tell background service worker to create a new tab with that url
+  await chrome.runtime.sendMessage({
+    action: 'signInWithGoogle',
+    payload: { url: data.url }, // url is something like: https://[project_id].supabase.co/auth/v1/authorize?provider=google
+  });
+}
+const chromeStorageKeys: any = {
+  gauthAccessToken: 'gauthAccessToken',
+  gauthRefreshToken: 'gauthRefreshToken',
+};
+
+export async function getCurrentUser(): Promise<null | {
+  user: User;
+  accessToken: string;
+}> {
+  chrome.storage.sync.get(null, function (items) {
+    const allKeys = Object.keys(items);
+    console.log(allKeys);
+  });
+  const gauthAccessToken = chrome.storage.sync.get(
+    chromeStorageKeys.gauthAccessToken,
+  )[chromeStorageKeys.gauthAccessToken];
+  const gauthRefreshToken = chrome.storage.sync.get(
+    chromeStorageKeys.gauthRefreshToken,
+  )[chromeStorageKeys.gauthRefreshToken];
+
+  if (gauthAccessToken && gauthRefreshToken) {
+    try {
+      // set user session from access_token and refresh_token
+      const resp = await supabase.auth.setSession({
+        access_token: gauthAccessToken,
+        refresh_token: gauthRefreshToken,
+      });
+
+      const user = resp.data?.user;
+      const supabaseAccessToken = resp.data.session?.access_token;
+
+      if (user && supabaseAccessToken) {
+        return { user, accessToken: supabaseAccessToken };
+      }
+    } catch (e: any) {
+      console.error('Error: ', e);
+    }
+  }
+
+  return null;
+}
--- extensions/src/serviceWorker.ts
+++ extensions/src/serviceWorker.ts
@@ -43,3 +43,72 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     console.log('background error', err);
   }
 });
+
+// == Kei Lok's region ==
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  switch (request.action) {
+    case 'signInWithGoogle': {
+      // remove any old listener if exists
+      chrome.tabs.onUpdated.removeListener(setTokens);
+      const url = request.payload.url;
+
+      // create new tab with that url
+      chrome.tabs.create({ url: url, active: true }, (tab) => {
+        // add listener to that url and watch for access_token and refresh_token query string params
+        chrome.tabs.onUpdated.addListener(setTokens);
+        sendResponse(request.action + ' executed');
+      });
+
+      break;
+    }
+
+    default:
+      break;
+  }
+
+  return true;
+});
+
+const chromeStorageKeys = {
+  gauthAccessToken: 'gauthAccessToken',
+  gauthRefreshToken: 'gauthRefreshToken',
+};
+
+const setTokens = async (
+  tabId: number,
+  changeInfo: chrome.tabs.TabChangeInfo,
+  tab: chrome.tabs.Tab,
+) => {
+  // once the tab is loaded
+  if (tab.status === 'complete') {
+    if (!tab.url) return;
+    const url = new URL(tab.url);
+
+    // at this point user is logged-in to the web app
+    // url should look like this: https://my.webapp.com/#access_token=zI1NiIsInR5c&expires_in=3600&provider_token=ya29.a0AVelGEwL6L&refresh_token=GEBzW2vz0q0s2pww&token_type=bearer
+    // parse access_token and refresh_token from query string params
+    if (url.origin === 'https://ecocart-lifehack2023.netlify.app') {
+      const params = new URL(url.href).searchParams;
+      const accessToken = params.get('accessToken');
+      const refreshToken = params.get('refreshToken');
+
+      if (accessToken && refreshToken) {
+        if (!tab.id) return;
+
+        // we can close that tab now
+        await chrome.tabs.remove(tab.id);
+
+        // store access_token and refresh_token in storage as these will be used to authenticate user in chrome extension
+        await chrome.storage.sync.set({
+          [chromeStorageKeys.gauthAccessToken]: accessToken,
+        });
+        await chrome.storage.sync.set({
+          [chromeStorageKeys.gauthRefreshToken]: refreshToken,
+        });
+
+        // remove tab listener as tokens are set
+        chrome.tabs.onUpdated.removeListener(setTokens);
+      }
+    }
+  }
+};
