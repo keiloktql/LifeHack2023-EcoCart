dd8dd29b5d6ffbeac581d2c51d207d2167727a18
Chai Pin Zheng <chaipinzheng@gmail.com>
2023-05-30 01:33:22 +0800

    feat: adding banner dynamically to product listings

--- extensions/src/foreground.ts
+++ extensions/src/foreground.ts
@@ -12,52 +12,75 @@ function getSpecificElement() {
   return document.querySelector('div.page-product__content');
 }
 
+function doesProductExistInDatabase() {
+  return false;
+}
+
 // Function to call on product page
 const runScriptProductPage = (function () {
-  return async function () {
-    // Variables
-    const wrapper = getWrapper();
-    const productInformation = {
-      categories: [],
-    };
-    const breadcrumbWrapper = wrapper.querySelector(
-      'div.page-product__breadcrumb',
-    );
-    const productWrapper = wrapper.querySelector('.page-product__detail > div');
-    const productRowAndColumn = productWrapper.querySelectorAll('div.dR8kXc');
+  return async function (wrapper) {
+    try {
+      // Variables
+      const productInformation = {
+        categories: [],
+      };
 
-    // Scripting
-    productInformation.categories = Array.from(
-      breadcrumbWrapper.querySelectorAll('a, span'),
-    ).map((breadcrumb) => {
-      return breadcrumb.innerText;
-    });
+      const breadcrumbWrapper = wrapper.querySelector(
+        'div.page-product__breadcrumb',
+      );
+      const productWrapper = wrapper.querySelector(
+        '.page-product__detail > div',
+      );
+      const productRowAndColumn = productWrapper.querySelectorAll('div.dR8kXc');
 
-    productInformation['Product Name'] =
-      productInformation.categories[productInformation.categories.length - 1];
-    Array.from(productRowAndColumn).forEach((row) => {
-      const title = row.querySelector('label').innerText;
-      const body =
-        (row.querySelector('div') && row.querySelector('div').innerText) || '';
+      // Scripting
+      productInformation.categories = Array.from(
+        breadcrumbWrapper.querySelectorAll('a, span'),
+      ).map((breadcrumb) => breadcrumb.innerText);
+      productInformation['Product Name'] =
+        productInformation.categories[productInformation.categories.length - 1];
 
-      // if the body is an empty string, it might be because it's a link / anchor tag
-      if (body === '') {
-        const anchor = row.querySelector('a');
-        if (anchor) {
-          productInformation[title] = anchor.innerText;
+      Array.from(productRowAndColumn).forEach((row) => {
+        const title = row.querySelector('label').innerText;
+        const body = row.querySelector('div')?.innerText || '';
+
+        // Check if the body is an empty string, it might be a link / anchor tag
+        if (body === '') {
+          const anchor = row.querySelector('a');
+          if (anchor) {
+            productInformation[title] = anchor.innerText;
+          }
+        } else {
+          productInformation[title] = body;
         }
-      } else {
-        productInformation[title] = body;
-      }
-    });
+      });
 
-    console.log(productInformation);
+      const productTitleWrapper = wrapper.querySelector('._44qnta');
+      document.querySelector('div[id="ecocart-flag"]')?.remove();
+      productTitleWrapper.parentElement.prepend(
+        await addReinforcement(doesProductExistInDatabase, productInformation),
+      );
+    } catch (err) {
+      console.log(err);
+    }
   };
 })();
 
 // Insert banner into the page
-const addReinforcement = async function (positive = true) {
-  console.log('HELLO');
+const addReinforcement = async function (
+  dataExistInDatabase = false,
+  productInformation = {},
+) {
+  const node = document.createElement('div');
+  node.innerHTML = `
+    <div id="ecocart-flag" class="ecocart-banner" style="border: 1px solid var(--petalc); color: var(--petalc); background: #CBF0C1; /* padding: 1rem; */ font: 0.9rem sans-serif; width: 100%; margin-bottom: 1rem; padding-left: 1rem; padding-top: 1rem; padding-bottom: 1rem; border-radius: 5px;">
+      <span>
+        ${dataExistInDatabase && '<img src="https://w7.pngwing.com/pngs/910/897/png-transparent-twitter-verified-badge-hd-logo.png" style="height: 17px; width: 17px;">'}
+        Did you the process of making '${productInformation['Product Name']}' produces <b>1000kg</b> of CO2? That is equivalent to driving a car (10km) or charging your phone (1000 times)!
+      </span>
+    </div>`;
+
+  return node;
 };
 
 const waitFor = function (varSetter, sleepTime, condition, continuation) {
@@ -85,7 +108,6 @@ const loadCss = function () {
 };
 
 (() => {
-  console.log('HIII');
   waitFor(
     getSpecificElement,
     1000,
@@ -93,7 +115,6 @@ const loadCss = function () {
       wrapper !== null &&
       wrapper.querySelector('.page-product__detail') !== null,
     function () {
-      console.log('THIS IS BEING CALLED');
       // Check if the url is a product page or search page by checking the url
       const url = window.location.href;
       const isSearchPage = url.includes('search');
